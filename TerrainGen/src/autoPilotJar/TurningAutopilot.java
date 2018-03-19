1aac8ff341c0b45488982d56b8b6e6ca1b940fd9
Unknown <torbengernaey@gmail.com>
2018-03-19 17:34:51 +0100

    Draaien lukt semi, hoogte behouden is problematisch

--- TerrainGen/src/autoPilotJar/TurningAutopilot.java
+++ TerrainGen/src/autoPilotJar/TurningAutopilot.java
@@ -16,7 +16,7 @@ public class TurningAutopilot {
 		this.height = 32; // p.getProperties().getPosition().getY();
 		System.out.println("HEIGHT: " + this.height);
 		
-		this.pidRoll = new PIDController(0.5f,0.0f,1.0f,(float) Math.toRadians(1),(float) Math.toRadians(10));
+		this.pidRoll = new PIDController(5.0f,0.0f,3.0f,(float) Math.toRadians(1),(float) Math.toRadians(10));
 		this.pidHorStab = new PIDController(1.1233587f, 0.30645216f, 1.1156111f, (float) (Math.PI / 180), 0);
 	}
 	
@@ -31,32 +31,34 @@ public class TurningAutopilot {
 			properties.setHorStabInclination((float) -(Math.PI / 6));
 		}
 
-		//ROLL
+
+		//ROLL AT 10 DEGREES
 		float changeWingRoll = this.pidRoll.calculateChange(properties.getRoll(),properties.getDeltaTime());
 		//System.out.println("Roll | ChangeWingRoll : " + this.inputAP.getRoll() + " | " + changeWingRoll);
 		
-		
 		properties.setLeftWingInclination(properties.getLeftWingInclination() + changeWingRoll);
 		if(properties.getLeftWingInclination() > Math.toRadians(15)) properties.setLeftWingInclination((float) Math.toRadians(15));
-		if(properties.getLeftWingInclination() < 0) properties.setLeftWingInclination(0);
+		if(properties.getLeftWingInclination() < Math.toRadians(-15)) properties.setLeftWingInclination((float) Math.toRadians(-15));
 		
 		properties.setRightWingInclination(properties.getRightWingInclination() - changeWingRoll);
-		if(properties.getRightWingInclination() < Math.toRadians(-15)) properties.setRightWingInclination((float) Math.toRadians(15));
-		if(properties.getRightWingInclination() < 0) properties.setRightWingInclination(0);
+		if(properties.getRightWingInclination() > Math.toRadians(15)) properties.setRightWingInclination((float) Math.toRadians(15));
+		if(properties.getRightWingInclination() < Math.toRadians(-15)) properties.setRightWingInclination((float) Math.toRadians(-15));
 		
 		System.out.println("+++++++++++");
 		System.out.println("Wing roll change : " + changeWingRoll);
-		System.out.println("ROLL: " + properties.getRoll());
+		System.out.println("ROLL: " + Math.toDegrees(properties.getRoll()));
 		System.out.println("LeftWingInc: " + properties.getLeftWingInclination());
 		System.out.println("RightWingInc: " + properties.getRightWingInclination());
-		System.out.println("+++++++++++");
+		
+		properties.setThrust(properties.getMaxThrust());
+		properties.setVerStabInclination(0);
 		
 		return properties;
 	}
 	
 	private float getVerAngle(DroneProperties properties) {
 		float overstaande = this.height - properties.getPosition().getY();
-		float aanliggende = 5;
+		float aanliggende = - 10;
 		return (float) Math.atan(overstaande / aanliggende);
 	}
 
