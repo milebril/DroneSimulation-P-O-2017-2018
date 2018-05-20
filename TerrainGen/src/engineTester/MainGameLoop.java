9c55181312d19f3d6d7222e06c7073edcdf1022c
Emil <milebril1@hotmail.com>
2018-05-20 21:46:30 +0200

    Een Mogelijke Demo

--- TerrainGen/src/engineTester/MainGameLoop.java
+++ TerrainGen/src/engineTester/MainGameLoop.java
@@ -132,7 +132,7 @@ public class MainGameLoop {
 //		module.defineAirport(0, -100, 50, 50);
 //		module.defineAirport(0, -200, 50, 50);
 		
-		//module.defineDrone(0, 0, 1, autopilotConfig);
+		module.defineDrone(0, 0, 1, autopilotConfig);
 		module.defineDrone(1, 0, 0, autopilotConfig);
 		//module.defineDrone(0, 0, 1, autopilotConfig);
 		//module.defineDrone(0, 1, 1, autopilotConfig);
