71867f06f1209606b70aea73ff1fa98e27be8146
Torben <torbengernaey@gmail.com>
2018-05-20 17:19:23 +0200

    2 drones op elkaar, andere oriëntatie

--- TerrainGen/src/engineTester/MainGameLoop.java
+++ TerrainGen/src/engineTester/MainGameLoop.java
@@ -166,7 +166,8 @@ public class MainGameLoop {
 //		module.defineAirport(0, -200, 50, 50);
 		
 		//module.defineDrone(0, 1, 1, autopilotConfig);
-		module.defineDrone(1, 0, 0, autopilotConfig);
+		module.defineDrone(0, 0, 0, autopilotConfig);
+		module.defineDrone(0, 0, 1, autopilotConfig);
 		//module.defineDrone(0, 1, 1, autopilotConfig);
 		
 //		for (int i = 0; i < 10; i++) {
